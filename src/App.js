1eed4df1934dc930e04d427ddfa376050a2cc5d6
Chris West (Faux) <git@goeswhere.com>
2017-05-06 14:06:09 +0200

    render a list (fuuuu)

--- src/App.js
+++ src/App.js
@@ -6,12 +6,55 @@ class App extends Component {
     return (
       <div className='app'>
         <div className='app-header'>
-          <h2>Welcome to React! </h2>
+          <h2>Interesting diffs:</h2>
         </div>
-        <p className='app-intro'>
-          To get started, edit <code>src/App.js</code>.
-        </p>
+        <div>
+          <Issues/>
+        </div>
       </div>
+    )
+  }
+}
+
+class Issues extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      fullList: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('static/intdiffs.json')
+      .then(response => {
+        response.json().then(fullList => {
+          this.setState({fullList})
+        })
+      })
+  }
+
+  render () {
+    return (
+      <table>
+        <tbody>
+          {this.state.fullList.map(row => {
+            var path, sub, before, after;
+            [path, sub, before, after] = row;
+            return (
+              <tr>
+                <td>{path}</td>
+                <td>{sub}</td>
+                <td>
+                  <pre>{before}</pre>
+                  <pre>{after}</pre>
+                </td>
+              </tr>
+            )
+          })
+          }
+        </tbody>
+      </table>
     )
   }
 }
